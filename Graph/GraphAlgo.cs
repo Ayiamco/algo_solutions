f86105ef86a26c17aecd25745f9c74a6d984c8c8
Joseph Achonu <josephachonu@gmail.com>
2024-02-16 10:37:04 +0100

    added new graph solutions

--- Graph/GraphAlgo.cs
+++ Graph/GraphAlgo.cs
@@ -11,8 +11,18 @@ public static class GraphAlgo
         {"e", null},
         {"f", null},
         {"g", new List<string> {"i", "j"}},
-         {"i", null},
+        {"i", null},
         {"j", null},
+        {"z", null},
+    };
+
+    static readonly List<List<string>> ExampleArrayOfEdges = new List<List<string>> 
+    {
+        new List<string> {"i", "j"},
+        new List<string> {"k", "i"},
+        new List<string> {"m", "k"},
+        new List<string> {"k", "l"},
+        new List<string> {"o", "n"},
     };
 
     public static void BreadthFirstTransversal(Dictionary<string, List<string>?>  graph, string start)
@@ -58,7 +68,7 @@ public static class GraphAlgo
     public static bool DepthFirstHasPath(Dictionary<string, List<string>?> graph, string src, string dest)
     {
         if(src == dest ) return true;
-        if(graph[src] == null) return false;
+        if(graph[src] == null || !graph[src].Any()) return false;
 
         foreach(var neighbour in graph[src]!)
         {
@@ -79,13 +89,13 @@ public static class GraphAlgo
 
         while(queue.Any())
         {
-            var currentEdge = queue.Dequeue();
+            var currentNode = queue.Dequeue();
 
-            if(currentEdge == dest) return true;
+            if(currentNode == dest) return true;
 
-            if(graph[currentEdge] is null) continue;
+            if(graph[currentNode] is null || !graph[currentNode].Any()) continue;
 
-            foreach(var neighbour in graph[currentEdge]!)
+            foreach(var neighbour in graph[currentNode]!)
             {
                 queue.Enqueue(neighbour);
             }
@@ -94,6 +104,88 @@ public static class GraphAlgo
         return false;
     }
 
+    /// <summary>
+    /// Write a function undirectedPath that takes an array of edges for an undirected graph and two nodes (nodeA, nodeB).
+    /// The function should return a boolean that indicates whether or not there exist a path between nodeA and nodeB.
+    /// Array of edges example = [[i,j],[j,k],[j,l]]
+    /// </summary>
+    public static bool BreadFirstUndirectedPath(List<List<string>> arrayOfEdges, string nodeA , string nodeB)
+    {
+        var undirectedGraph = ConvertArrayOfEdgesToGraph(arrayOfEdges);
+
+        var visitedSet = new HashSet<string>();
+
+        return HasPath(undirectedGraph,nodeA, nodeB, visitedSet);
+    }
+
+    /// <summary>
+    /// A set of nodes forms a connected component in an undirected graph if any node from the set of nodes can reach any other node by traversing edges.
+    /// Write a function ConnectedComponentsCount that takes the adjacency list of an undirected graph. The function should return the number of connected components
+    /// withing the graph
+    /// </summary>
+    /// <returns></returns>
+    public static int GetConnectedComponentsCount(Dictionary<string, List<string>?> graph)
+    {
+        var connectedComponentCount = 0;
+        var visitedNodes = new HashSet<string> ();
+        foreach(var node in graph.Keys)
+        {
+            if(visitedNodes.Any(x => x.Equals(node)))
+                continue;
+
+            TransverseConnectedComponentsCount(graph,node,visitedNodes);
+            connectedComponentCount++;
+        }
+
+        return connectedComponentCount;
+    }
+
+    public static void TransverseConnectedComponentsCount (Dictionary<string, List<string>?> graph, string startNode, HashSet<string> visitedNodes)
+    {
+        if(!visitedNodes.TryGetValue(startNode,out string val))
+            visitedNodes.Add(startNode);
+
+        if(graph[startNode] == null) return;
+
+        foreach(var edge in graph[startNode])
+        {
+            TransverseConnectedComponentsCount(graph,edge,visitedNodes);
+        }
+    }
+
+    public static bool HasPath(Dictionary<string, List<string>?> graph, string src, string dest, HashSet<string> visitedNodes)
+    {
+        if(visitedNodes.TryGetValue(src,out string val)) return false;
+
+        visitedNodes.Add(src);
+
+        if(src == dest ) return true;
+
+        if(graph[src] == null || !graph[src].Any()) return false;
+
+        foreach(var neighbour in graph[src]!)
+        {
+            if(HasPath(graph, neighbour, dest,visitedNodes))
+                return true;
+        }
+        return false;
+    }
+
+    public static Dictionary<string, List<string>?> ConvertArrayOfEdgesToGraph(List<List<string>> arrayOfEdges)
+    {
+        var adjacencyList = new  Dictionary<string, List<string>?>();
+        foreach(var edges in arrayOfEdges)
+        {
+            foreach(var edge in edges)
+            {
+                if(!adjacencyList.TryGetValue(edge, out List<string> value))
+                    adjacencyList[edge] = new List<string>();
+
+                adjacencyList[edge]!.AddRange(edges.Where( x=> x != edge));
+            }
+        }
+        return adjacencyList;
+    }
     public static void TestGraphAlgo()
     {
         BreadthFirstTransversal(ExampleGraph, "a");
@@ -102,5 +194,15 @@ public static class GraphAlgo
         var breadthFirstHasPath = BreadthFirstHasPath(ExampleGraph, "a","j");
         Console.WriteLine($"depthFirstHasPath => Expected:True; Actual:{depthFirstHasPath}");
         Console.WriteLine($"breadthFirstHasPath => Expected:True; Actual:{breadthFirstHasPath}");
+
+        var pathExist = BreadFirstUndirectedPath(ExampleArrayOfEdges, "i","m");
+        Console.WriteLine($"BreadFirstUndirectedPath => Expected:True; Actual:{pathExist}");
+
+        pathExist = BreadFirstUndirectedPath(ExampleArrayOfEdges, "i","o");
+        Console.WriteLine($"BreadFirstUndirectedPath => Expected:False; Actual:{pathExist}");
+
+        var connectedComponentCount = GetConnectedComponentsCount(ExampleGraph);
+        Console.WriteLine(connectedComponentCount);
+
     }
 }
